a512b0cec2a663bc89bc0c2ea504d47d3f7c6746
Egoscio <theiqboy@gmail.com>
2017-08-14 23:39:58 -0700

    Adding Project
    Adding Unity Project to the Repository

---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+
+	public TankTurret_Pivot Pivot;
+	
+	private void Awake()
+	{
+
+	}
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(Vector3.forward);
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(Vector3.back);
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(Vector3.down);
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(Vector3.up);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			Pivot.transform.Rotate(Vector3.down);
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			Pivot.transform.Rotate(Vector3.up);
+		}
+	}
+
+
+}
